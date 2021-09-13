c8deaa43a7830dfa97d487971379c45f2367bcac
Chris <oaoxd0314@gmail.com>
2021-09-13 18:47:06 +0800

    (2021 09 13) eslint 去除 windows 的奇怪錯誤

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     "no-console": "error",//禁止 console，這個很常在開發中大量使用，卻忘記消掉XD 
     "no-empty": "error",//禁止無意義的空白，可能會破壞排版，見仁見智啦
     "no-irregular-whitespace":"error",
-    "semi":"off" //關掉 ; 檢查
+    "semi":"off", //關掉 ; 檢查
+    'linebreak-style': ['error', 'window'],
   },
 };
